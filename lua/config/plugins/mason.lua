dfa34cb8dfa73083f99a7a91fd57dde6daeea0c7
asters1 <gj744559140@163.com>
2023-07-11 06:56:24 +0000

    更新

--- lua/config/plugins/mason.lua
+++ lua/config/plugins/mason.lua
@@ -1,30 +1,32 @@
 -- 就这一个页面就能自动安装,不能安装的话是网络问题
 return {
-	gh.."williamboman/mason-lspconfig.nvim",
+	gh .. "williamboman/mason-lspconfig.nvim",
 	lazy = false,
 	dependencies = {
-		{gh.. "williamboman/mason.nvim", build = ":MasonUpdate", },
+		{ gh .. "williamboman/mason.nvim", build = ":MasonUpdate" },
 	},
 	config = function()
-    vim.keymap.set('n', '<leader>m',':Mason<CR>')
+		vim.keymap.set("n", "<leader>m", ":Mason<CR>")
 		require("mason").setup({
 			ui = {
 				icons = {
 					package_installed = "✓",
 					package_pending = "➜",
-					package_uninstalled = "✗"
-				}
-			}
+					package_uninstalled = "✗",
+				},
+			},
 		})
 		require("mason-lspconfig").setup({
-			ensure_installed = { 
-        "lua_ls",
-        "gopls",
-        "bashls",
-        "jsonls",
-        "pyright"
-      },
+			ensure_installed = {
+				--lsp
+				"lua_ls",
+				"gopls",
+				"bashls",
+				"jsonls",
+				"pyright",
+				--format
+			},
 			automatic_installation = true,
 		})
-	end
+	end,
 }
